748d1e0afc2b29a0edfea6391ecac82dc9e15e6f
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2013-10-29 18:47:31 +0200

    adds Powerline to bash and IPython

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -44,7 +44,7 @@ c = get_config()
 # c.InteractiveShellApp.pylab_import_all = True
 
 # A list of dotted module names of IPython extensions to load.
-# c.InteractiveShellApp.extensions = []
+c.InteractiveShellApp.extensions = ['powerline.bindings.ipython.post_0_11']
 
 # Run the module as a script.
 # c.InteractiveShellApp.module_to_run = ''
