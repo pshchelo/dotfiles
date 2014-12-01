3038a9c9988b13613deab8281eb8c7cb73abe50d
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2014-12-01 11:43:11 +0000

    Change default IP range for libvirt pool

--- scripts/setiptables.sh
+++ scripts/setiptables.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 myip=$(hostname -i)
 # DevStack
-iptables -t nat -I PREROUTING -p tcp -d $myip --dport 8127 -j DNAT --to-destination 192.168.122.127:80
-iptables -I FORWARD -m state -d 192.168.122.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -I PREROUTING -p tcp -d $myip --dport 8127 -j DNAT --to-destination 192.168.100.127:80
+iptables -I FORWARD -m state -d 192.168.100.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
 # Fuel master
 #iptables -t nat -I PREROUTING -p tcp -d $myip --dport 8888 -j DNAT --to-destination 10.108.0.2:80
 #iptables -I FORWARD -m state -d 10.108.0.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
