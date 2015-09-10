6907458aa58a767c533a7262b2c4a2533643adf7
Pavlo Shchelokovskyy <pshchelokovskyy@mirantis.com>
2015-09-10 10:48:03 +0000

    Add ports for Jupyter etc

--- scripts/setiptables.sh
+++ scripts/setiptables.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env sh
 myip=$(hostname -i)
-# DEVSTACK
-# ssh to the devstack host
+
+# Devstack-Heat
+# ssh access
 iptables -t nat -I PREROUTING -p tcp -d $myip --dport 2127 -j DNAT --to-destination 192.168.100.127:22
-# Horizon access to the devstack host
+# Horizon access
 iptables -t nat -I PREROUTING -p tcp -d $myip --dport 8127 -j DNAT --to-destination 192.168.100.127:80
 # noVNC console access in Horizon
 iptables -t nat -I PREROUTING -p tcp -d $myip --dport 6127 -j DNAT --to-destination 192.168.100.127:6080
 # xvpVNC tunnel
 iptables -t nat -I PREROUTING -p tcp -d $myip --dport 7127 -j DNAT --to-destination 192.168.100.127:6081
+# Jupyter notebook server access
+iptables -t nat -I PREROUTING -p tcp -d $myip --dport 8827 -j DNAT --to-destination 192.168.100.127:8888
+# Jupyter nbconvert slides access
+iptables -t nat -I PREROUTING -p tcp -d $myip --dport 9927 -j DNAT --to-destination 192.168.100.127:9999
 
-# ssh to the ironic host
+
+# Devstack-Ironic
+# ssh access
 iptables -t nat -I PREROUTING -p tcp -d $myip --dport 2126 -j DNAT --to-destination 192.168.100.126:22
-
-iptables -I FORWARD -m state -d 192.168.100.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
 
 # Fuel master
 #iptables -t nat -I PREROUTING -p tcp -d $myip --dport 8888 -j DNAT --to-destination 10.108.0.2:80
@@ -21,3 +26,6 @@ iptables -I FORWARD -m state -d 192.168.100.0/24 --state NEW,RELATED,ESTABLISHED
 # Fuel env 1 Public network
 #iptables -t nat -I PREROUTING -p tcp -d $myip --dport 8182 -j DNAT --to-destination 10.108.1.2:80
 #iptables -I FORWARD -m state -d 10.108.1.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
+
+# General access
+iptables -I FORWARD -m state -d 192.168.100.0/24 --state NEW,RELATED,ESTABLISHED -j ACCEPT
