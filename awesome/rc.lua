4f1f223410ac7910a85c56862c4387d4f9092e25
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2016-01-28 14:54:30 +0200

    Use personal awesome theme

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -41,7 +41,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/themes/solarized-dark-default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 --terminal = "x-terminal-emulator"
