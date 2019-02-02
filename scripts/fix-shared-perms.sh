26517cb75290c94501ed1ce5cf8d410bc99aa748
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2019-02-02 14:05:50 +0200

    Improve shared perm script

--- scripts/fix-shared-perms.sh
+++ scripts/fix-shared-perms.sh
@@ -3,6 +3,6 @@ sudo -v
 fixdir=${1:-$PWD}
 fixgrp=${2:-shared}
 sudo chgrp -R $fixgrp "${fixdir}"
-sudo chmod g+w "${fixdir}"
-sudo find "${fixdir}" -type d -exec chmod g+ws '{}' +
-sudo find "${fixdir}" -type f -exec chmod g+w '{}' +
+sudo chmod -R g+rw,o-rwx "${fixdir}"
+sudo find "${fixdir}" -type d -exec chmod g+s '{}' +
+sudo find "${fixdir}" -type f \( -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.arw' \) -exec chmod -x '{}' +
