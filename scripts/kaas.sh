3999040a80bda478a1080dee3fd4b1c85dfb0211
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2019-07-19 14:26:43 +0300

    Improve kaas.sh script
    can now show IP address of a node with a given role in the cluster

--- scripts/kaas.sh
+++ scripts/kaas.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 cluster=$1
-project=${2:-oscore-team}
-context=${3:-pshchelokovskyy@kaas}
-kubectl --context ${context} -n imc-${project} get machine -l cluster.k8s.io/cluster-name=${cluster} -ojson | jq '.items[].metadata | {name: .name, ip: .annotations."openstack-ip-address", role: .labels.set, id: .annotations."openstack-resourceId"}'
+role=${2:-}
+realm=${KAAS_REALM:-imc-oscore-team}
+context=${KAAS_CONTEXT:-pshchelokovskyy@kaas}
+cluster=`kubectl --context ${context} -n ${realm} get machine -l cluster.k8s.io/cluster-name=${cluster} -ojson`
+if [ -z $role ]; then
+    echo $cluster | jq '.items[].metadata | {name: .name, ip: .annotations."openstack-ip-address", role: .labels.set, id: .annotations."openstack-resourceId"}'
+else
+    echo $cluster | jq -r ".items[].metadata | select(.labels.set==\"$role\") | .annotations.\"openstack-ip-address\""
+fi
