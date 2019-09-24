3c5656482ecc6f82cc55209707d4f707123d8628
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2019-09-24 10:04:41 +0300

    Improve kaas.sh script
    allow immediate SSH into KaaS instances deployed in our devcloud

--- scripts/kaas.sh
+++ scripts/kaas.sh
@@ -8,5 +8,10 @@ cluster=`kubectl --context ${context} -n ${realm} get machine -l cluster.k8s.io/
 if [ -z $role ]; then
     echo $cluster | jq '.items[].metadata | {name: .name, ip: .annotations."openstack-ip-address", role: .labels.set, id: .annotations."openstack-resourceId"}'
 else
-    echo $cluster | jq -r ".items[].metadata | select(.labels.set==\"$role\") | .annotations.\"openstack-ip-address\""
+    node_ip=$(echo $cluster | jq -r ".items[].metadata | select(.labels.set==\"$role\") | .annotations.\"openstack-ip-address\"")
+    if [ $(basename ${BASH_SOURCE[0]}) = 'kaassh' ]; then
+        ssh -l ubuntu -i ~/.ssh/aio_rsa $node_ip
+    else
+        echo $node_ip
+    fi
 fi
