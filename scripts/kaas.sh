1d5e456779ab77bb833aa7b74609983f01ab7424
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2019-10-18 13:29:34 +0300

    Revert kaas.sh to simple SSH only
    k9s is veeery slow over my sshuttle connection

--- scripts/kaas.sh
+++ scripts/kaas.sh
@@ -3,26 +3,17 @@ set -e
 
 function print_help {
     echo "$0 - operations on KaaS cluster"
-    echo "Usage: $0 CLUSTER_NAME [--list|--ssh] [OPTIONS]"
+    echo "Usage: $0 CLUSTER_NAME [--list|--ssh]"
     echo "ENV vars: KAAS_REALM, KAAS_CONTEXT, KAAS_PRIVATE_KEY"
 }
 
-function sshuttle_to_cluster {
-    k9s_args=$@
+function ssh_to_master_node {
     node_ip=$(${kubecmd} get machine -l cluster.sigs.k8s.io/cluster-name=${cluster} -ojson | jq -r '.items[].metadata | select(.labels."cluster.sigs.k8s.io/control-plane"=true) | .annotations."openstack-ip-address"' | head -n1)
     if [ -z $node_ip ]; then
         echo "No control plane nodes found for cluster $cluster"
         exit 1
     fi
-    ssh -i ${ssh_key} ubuntu@$node_ip "sudo cp -R /root/.kube /home/ubuntu/.kube && sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube"
-    scp -i ${ssh_key} ubuntu@$node_ip:~/.kube/config kaas-child-config
-    subnet_cidr=$(${kubecmd} get cluster ${cluster} -o json | jq -r '.status.providerStatus.network.subnet.cidr')
-    sshuttle -D -r ubuntu@$node_ip -e "ssh -i ${ssh_key}" $subnet_cidr
-    set -x
-    k9s --kubeconfig kaas-child-config $k9s_args
-    if [ -f sshuttle.pid ]; then
-        kill $(cat sshuttle.pid)
-    fi
+    ssh -i ${ssh_key} ubuntu@$node_ip
 }
 
 function list_cluster_nodes {
@@ -46,7 +37,7 @@ shift
 case $1 in
     "--ssh")
         shift
-        sshuttle_to_cluster $*
+        ssh_to_master_node $*
         ;;
     "--list")
         shift
