2804cf2577afa4471ca3c5d75b563874a94c1ff5
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2020-04-10 13:55:10 +0000

    [k8s] pull names of serices from ingress

--- k8s/openstackk8sfqdn.sh
+++ k8s/openstackk8sfqdn.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 ingress_cluster_ip=$(kubectl -n openstack get svc ingress -ojsonpath='{.spec.clusterIP}')
-for svc in keystone glance cinder nova neutron horizon designate placement octavia heat cloudformation barbican; do
-    echo "${ingress_cluster_ip} ${svc}.it.just.works" >> /etc/hosts
+echo "" >> /etc/hosts
+echo "# openstack-on-k8s services" >> /etc/hosts
+for svc in $(kubectl -n openstack get ingress |awk '/cluster-fqdn/ {print $2}'); do
+    echo "${ingress_cluster_ip} ${svc}" >> /etc/hosts
 done
