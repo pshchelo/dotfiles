ce44efcb6c1ee40ca8a21a3ee2c2c3d0e37118bc
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2020-07-28 16:44:45 +0000

    Allow using lb ip for ingress for /etc/hosts

--- k8s/openstackk8sfqdn.sh
+++ k8s/openstackk8sfqdn.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+ingress_lb_ip=$(kubectl -n openstack get svc ingress -ojsonpath='{.status.loadBalancer.ingress[0].ip}')
 ingress_cluster_ip=$(kubectl -n openstack get svc ingress -ojsonpath='{.spec.clusterIP}')
 echo "" >> /etc/hosts
 echo "# openstack-on-k8s services" >> /etc/hosts
 for svc in $(kubectl -n openstack get ingress |awk '/cluster-fqdn/ {print $2}'); do
-    echo "${ingress_cluster_ip} ${svc}" >> /etc/hosts
+    echo "${ingress_lb_ip} ${svc}" >> /etc/hosts
+    #echo "${ingress_cluster_ip} ${svc}" >> /etc/hosts
 done
 echo "# end of openstack services" >> /etc/hosts
