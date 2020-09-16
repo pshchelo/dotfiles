f1812c2e413a35665aaed831bdc0952b23182d47
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2020-09-16 12:08:08 +0000

    Adapt to new kubectl output format

--- k8s/openstackk8sfqdn.sh
+++ k8s/openstackk8sfqdn.sh
@@ -4,7 +4,7 @@ ingress_lb_ip=$(kubectl -n openstack get svc ingress -ojsonpath='{.status.loadBa
 ingress_cluster_ip=$(kubectl -n openstack get svc ingress -ojsonpath='{.spec.clusterIP}')
 echo "" >> /etc/hosts
 echo "# openstack-on-k8s services" >> /etc/hosts
-for svc in $(kubectl -n openstack get ingress |awk '/cluster-fqdn/ {print $2}'); do
+for svc in $(kubectl -n openstack get ingress |awk '/cluster-fqdn/ {print $3}'); do
     echo "${ingress_lb_ip} ${svc}" >> /etc/hosts
     #echo "${ingress_cluster_ip} ${svc}" >> /etc/hosts
 done
