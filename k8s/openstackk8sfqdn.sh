5c24c7c15f8b86cb9192282633b3a3dc7786909b
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2022-02-11 16:08:33 +0200

    Simplify mosk fqdn script
    do not actually write to /etc/hosts, let the user do it herself
    ( | sudo tee -a ..)
    also simplify kubectl calls, use jsonpath instead of piping to awk

--- k8s/openstackk8sfqdn.sh
+++ k8s/openstackk8sfqdn.sh
@@ -2,10 +2,7 @@
 
 ingress_lb_ip=$(kubectl -n openstack get svc ingress -ojsonpath='{.status.loadBalancer.ingress[0].ip}')
 ingress_cluster_ip=$(kubectl -n openstack get svc ingress -ojsonpath='{.spec.clusterIP}')
-echo "" >> /etc/hosts
-echo "# openstack-on-k8s services" >> /etc/hosts
-for svc in $(kubectl -n openstack get ingress |awk '/cluster-fqdn/ {print $3}'); do
-    echo "${ingress_lb_ip} ${svc}" >> /etc/hosts
-    #echo "${ingress_cluster_ip} ${svc}" >> /etc/hosts
+for svc in $(kubectl -n openstack get ingress -ojsonpath='{.items[*].spec.rules[*].host}'); do
+    echo "${ingress_lb_ip} ${svc}"
+    #sudo echo "${ingress_cluster_ip} ${svc}"
 done
-echo "# end of openstack services" >> /etc/hosts
