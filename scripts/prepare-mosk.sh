83ad6db2ae75189cea64443381a109153188dacd
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2022-06-04 20:08:22 +0300

    Change prepare-mosk scritp to work w/o tunnel

--- scripts/prepare-mosk.sh
+++ scripts/prepare-mosk.sh
@@ -1,6 +1,6 @@
-openstack flavor create m1.nano --ram 128 --disk 1 --vcpu 1
-openstack user create demo --password admin --domain default
-openstack user create alt_demo --password admin --domain default
-openstack project create demo --domain default
-openstack role add member --user demo --user-domain default --project demo --project-domain default
-openstack role add member --user alt_demo --user-domain default --project demo --project-domain default
+kopenstack flavor create m1.nano --ram 128 --disk 1 --vcpu 1
+kopenstack user create demo --password admin --domain default
+kopenstack user create alt_demo --password admin --domain default
+kopenstack project create demo --domain default
+kopenstack role add member --user demo --user-domain default --project demo --project-domain default
+kopenstack role add member --user alt_demo --user-domain default --project demo --project-domain default
