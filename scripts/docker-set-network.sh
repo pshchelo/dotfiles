e9196e74c859637254dd0983ddc8739fc798cc7b
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2023-08-17 13:26:14 +0000

    Make docker network changer a proper script

---
+++ scripts/docker-set-network.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+new_ip_with_cidr="${1}"
+docker_br=${2:-"docker0"}
+docker_daemon_config=/etc/docker/daemon.json
+current_bridge_ip=$(ip -br -f inet addr show dev $docker_br | awk '{print $3}')
+sudo systemctl stop docker.service
+sudo ip link set dev $docker_br down
+sudo ip addr del $current_bridge_ip dev $docker_br
+if [ -f $docker_daemon_config ]; then
+    sudo cp $docker_daemon_config $docker_daemon_config.bak
+fi
+echo "{\"bip\": \"$new_ip_with_cidr\"}" | sudo tee $docker_daemon_config
+sudo ip addr add $new_ip_with_cidr dev $docker_br
+sudo ip link set dev $docker_br up
+sudo systemctl start docker.service
