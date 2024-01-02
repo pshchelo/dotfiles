44f3d57fffb80bf854f3585374cf959f1fca8bbd
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-01-02 22:32:58 +0200

    Add steps logging to ubuntu-update-all

--- scripts/ubuntu-update-all.sh
+++ scripts/ubuntu-update-all.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+echo "=== updating package repos ==="
 sudo apt update
+echo "=== upgrading packages ==="
 sudo apt upgrade
+echo "=== removing no longer used packages ==="
 sudo apt autoremove
 if command -v snap > /dev/null; then
+    echo "=== updating snaps ==="
     sudo snap refresh
 fi
 if command -v flatpak > /dev/null; then
+    echo "=== updating flatpak apps ==="
     sudo flatpak update
 fi
 if [ -f /var/run/reboot-required ]; then
