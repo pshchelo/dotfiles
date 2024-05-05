f48e154961cbdb8474e2b9e2135252cb4fabb4b3
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-05-05 22:53:32 +0300

    Add brew bootstrap script

---
+++ scripts/brew-install.sh
@@ -0,0 +1,13 @@
+# see https://brew.sh/
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install \
+    starship \
+    jq \
+    fzf \
+    fd \
+    bat \
+    lf \
+    ripgrep \
+    k9s \
+    neovim
