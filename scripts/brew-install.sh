7fa072714631157dd1a84a9a9b37ef225eb8b869
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-05-05 23:59:00 +0300

    Add hint on brew-installed apps and sudo

--- scripts/brew-install.sh
+++ scripts/brew-install.sh
@@ -11,3 +11,9 @@ brew install \
     ripgrep \
     k9s \
     neovim
+
+# for a brew-installed program to be available for sudo, it needs to be
+# in `secure_path` paths (https://github.com/orgs/Homebrew/discussions/3575):
+# - either add simlinks to standard place like /usr[/local/]/[s]bin 
+# - or run `sudo visudo` and append to the secure_path
+# :/home/linuxbrew/.linuxbrew/sbin:/home/linuxbrew/.linuxbrew/bin
