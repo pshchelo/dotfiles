9baa08e0a3b31ded9632afa7ac7a5e3466d3aab6
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-09-09 09:45:27 +0000

    Update brew install insctuctions for Linux
    mention how to deal with 'passwordless sudo' machines like
    developer VMs

--- scripts/brew-install.sh
+++ scripts/brew-install.sh
@@ -1,19 +1,32 @@
+# #!/usr/bin/env bash
+
+# For linux machine with passwordless sudo,
+# manuallly pre-create the brew install prefix dir,
+# otherwise the install script will keep asking for the password,
+# for default prefix this looks like:
+
+#brew_dir="/home/linuxbrew/.linuxbrew"
+#me=$(whoami)
+#sudo mkdir -p "$brew_dir" && sudo chown $me:$me "$brew_dir"
+
 # see https://brew.sh/
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+
 brew install \
-    starship \
-    jq \
-    fzf \
-    fd \
     bat \
-    lf \
-    ripgrep \
+    fd \
+    fzf \
+    jq \
     k9s \
-    neovim
+    lf \
+    mosh \
+    neovim \
+    ripgrep \
+    starship
 
 # for a brew-installed program to be available for sudo, it needs to be
 # in `secure_path` paths (https://github.com/orgs/Homebrew/discussions/3575):
-# - either add simlinks to standard place like /usr[/local/]/[s]bin 
+# - either add simlinks to standard place like /usr[/local/]/[s]bin
 # - or run `sudo visudo` and append to the secure_path
 # :/home/linuxbrew/.linuxbrew/sbin:/home/linuxbrew/.linuxbrew/bin
