e30c97c2d84c4798c3ba244bf9562752a5b3ac3e
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-09-23 20:44:26 +0300

    Update nvim install script

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$(dirname $0)
 sudo cp $SCRIPT_DIR/nview $SCRIPT_DIR/nvimdiff /usr/local/bin/
+sudo ln -s $(which nvim) /usr/local/bin/nvim
 for cmd in vim view vimdiff; do
     sudo update-alternatives --install $(which $cmd) $cmd $(which n$cmd) 100
 done
