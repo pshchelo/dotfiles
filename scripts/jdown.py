66448e858e80615c3207b9e24c64de0ce779731d
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-01-14 12:32:15 +0000

    Improve jdown.py script
    - make proper CLI with argparse
    - split attachment discovery and filtering into separate func

--- scripts/jdown.py
+++ scripts/jdown.py
@@ -1,45 +1,80 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import pathlib
 import sys
 import requests
 """Download Jira issue attachments from CLI."""
+
 # TODO: proper config load from some JSON in ~/.config or smth
-JIRA_SERVER = os.getenv("JIRA_SERVER")
-JIRA_USERNAME = os.getenv("JIRA_USERNAME")
-JIRA_TOKEN = os.getenv("JIRA_TOKEN")
-JIRA_API_VERSION = 2
-JIRA_CHUNK_SIZE = 1024 * 1024  # chunk size for downloads (in bytes)
-JIRA_TIMEOUT = 60
-JIRA_API = JIRA_SERVER.strip("/") + f"/rest/api/{JIRA_API_VERSION}"
-# TODO: use proper argparse
-if len(sys.argv) < 2:
-    sys.exit("Need issue ID")
-issue_id = sys.argv[1].upper()
-attachment_names = sys.argv[2:]
-s = requests.Session()
-s.auth = (JIRA_USERNAME, JIRA_TOKEN)
-# TODO: interactive mode, list available attachments and ask what to download
-# TODO: some progress bar / work being done feedback
-# TODO: do not overwrite existing files, may be check integrity somehow first
-resp = s.get(JIRA_API + f"/issue/{issue_id}", timeout=JIRA_TIMEOUT)
-if resp.status_code != 200:
-    sys.exit(f"{resp.status_code}: {resp.text}")
-attachments = resp.json()["fields"]["attachment"]
-if not attachments:
-    sys.exit(f"Issue {issue_id} has no attachments. Wrong issue id?")
-# FIXME: could there be duplicate filename but different attachment id?
-if not attachment_names:
-    download_dir = pathlib.Path(issue_id)
-    os.makedirs(download_dir, exist_ok=True)
-else:
-    download_dir = pathlib.Path(".")
-for a in attachments:
-    fname = a["filename"]
-    if attachment_names and fname not in attachment_names:
-        continue
-    print(f"downloading {fname}")
-    with open(download_dir / fname, "wb") as f:
-        with s.get(a["content"], stream=True, timeout=JIRA_TIMEOUT) as r:
-            for chunk in r.iter_content(chunk_size=JIRA_CHUNK_SIZE):
-                f.write(chunk)
+def make_cli():
+    parser = argparse.ArgumentParser(
+        prog="jdown",
+        description="Download Jira ticket attachments via CLI"
+    )
+    parser.add_argument("issue", help="Jira issue (case-insensitive)")
+    parser.add_argument("attachments", nargs="*",
+                        help="Attachment names. If none specified, all existing "
+                             "attachments for the given issue will be downloaded")
+    parser.add_argument("--jira-version", default="2",
+                        help="Jira API version (default is 2)")
+    parser.add_argument("--jira-server", default=os.getenv("JIRA_SERVER"),
+                        help="URL of Jira server, defaults to JIRA_SERVER env var")
+    parser.add_argument("--jira-username", default=os.getenv("JIRA_USERNAME"),
+                        help="Jira user name, defaults to JIRA_USERNAME env var")
+    parser.add_argument("--jira-token", default=os.getenv("JIRA_TOKEN"),
+                        help="Jira API token, defaults to JIRA_TOKEN env var")
+    parser.add_argument("--jira-timeout", type=int, default=60,
+                        help="HTTP timeout for Jira API, in seconds, "
+                             "defaults to 60.")
+    parser.add_argument("--jira-chunk_size", type=int, default=1024*1024,
+                         help="Downolad chunk size in bytes, defaults to 1MB")
+    return parser.parse_args()
+
+
+def discover_attachments(args, session):
+    resp = session.get(
+        args.jira_server.strip("/")
+        + f"/rest/api/{args.jira_version}"
+        + f"/issue/{args.issue}",
+        timeout=args.jira_timeout,
+    )
+    if resp.status_code != 200:
+        sys.exit(f"{resp.status_code}: {resp.text}")
+    attachments = resp.json()["fields"]["attachment"]
+    if args.attachments:
+        return [a for a in attachments if a["filename"] in args.attachments]
+    else:
+        return attachments
+
+
+def jdown(args):
+    s = requests.Session()
+    s.auth = (args.jira_username, args.jira_token)
+
+    attachments = discover_attachments(args, s)
+    if not attachments:
+        sys.exit(f"Issue {args.issue} has no requested attachments.")
+
+    # TODO: interactive mode, list available attachments and ask what to download
+    # TODO: some progress bar / work being done feedback
+    # TODO: do not overwrite existing files, may be check integrity somehow first
+    # TODO: download in parallel
+    # FIXME: could there be duplicate filename but different attachment id?
+    if not args.attachments:
+        download_dir = pathlib.Path(args.issue)
+        os.makedirs(download_dir, exist_ok=True)
+    else:
+        download_dir = pathlib.Path(".")
+    for a in attachments:
+        fname = a["filename"]
+        print(f"downloading {fname}")
+        with open(download_dir / fname, "wb") as f:
+            with s.get(
+                a["content"], stream=True, timeout=args.jira_timeout
+            ) as r:
+                for chunk in r.iter_content(chunk_size=args.jira_chunk_size):
+                    f.write(chunk)
+
+if __name__ == "__main__":
+    jdown(make_cli())
