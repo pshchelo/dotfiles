77c2a96dce450948489902ad6c618b51458313f5
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2025-05-26 10:52:13 +0000

    Move from pipx to uv
    only leaving ansible and pylsp under pipx
    as uv has no equivalent to `--install-deps` yet.

--- scripts/pipx-install.sh
+++ scripts/pipx-install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # TODO: re-implement in Ansible using https://docs.ansible.com/ansible/latest/collections/community/general/pipx_module.html
+# NOTE: these are the python-based tools that are not yet convenient yet
+# to install with `uv` https://github.com/astral-sh/uv/issues/6314
 
 function install_pipx {
     # TODO: check if new enough pipx is present in apt repos,
@@ -8,21 +10,8 @@ function install_pipx {
     pipx ensurepath
 }
 
-function install_main {
+function install_lsp {
     # Python and others development tools
-    pipx install \
-        bindep \
-        flake8 \
-        git-review \
-        pre-commit \
-        shellcheck-py \
-        sshuttle \
-        tox \
-        twine \
-        yq
-    # curl with human face
-    pipx install httpie
-    pipx inject httpie httpie-keystone-auth
     # python-lsp-server and plugins, expose plugins as tools
     pipx install python-lsp-server
     pipx inject python-lsp-server \
@@ -46,102 +35,32 @@ function install_main {
     #pyls-isort: code formatting using isort (automatic import sorting).
 }
 
-function install_osc {
-    # installs a pipx env with python-openstackclient and all plugins
-    # for services supported in MOSK
-    # the rest are either non-OpenStack services/components,
-    # OpenStack but do not have HTTP API (like metering (ceilometer)),
-    # or are deprecated in MOSK (like events(panko))
-    pipx install python-openstackclient # supports Keystone, Nova, Glance, Cinder, Neutron, Swift
-    pipx inject python-openstackclient \
-        aodhclient \
-        gnocchiclient \
-        osc-placement \
-        python-barbicanclient \
-        python-designateclient \
-        python-heatclient \
-        python-ironicclient \
-        python-manilaclient \
-        python-masakariclient \
-        python-octaviaclient # this one better from downstream for support of force-delete
-    openstack complete > ~/.local/share/bash-completion/completions/openstack
-}
-
-function install_demotools {
-    # demo recording helpers
-    pipx install asciinema
-    pipx install demoshell
-    pipx install doitlive
-}
 
 function install_ansible {
     pipx install ansible --include-deps
     pipx inject ansible openstacksdk --include-apps
 }
 
-function install_optional {
-    # file manager a-la lf but in Python
-    pipx install ranger-fm
-    # markdown files viewer
-    pipx install frogmouth
-    # log viewer
-    pipx install toolong
-}
-
-function install_dbclients {
-    # enhanced DB CLI's - https://www.dbcli.com
-    pipx instal \
-        pgcli \
-        litecli \
-        iredis \
-        mycli
-}
-
-function install_guitools {
-    # GUI
-    # posterize pdf or images to multiple sheets that can be glued together
-    pipx install plakativ
-    # Steam's proton enhancements
-    pipx install protontricks
-    # rst/md editor with live HTML preview
-    pipx install retext
-}
-
 __usage="
-Usage: $(basename "$0") [-hvimodbatpg]
+Usage: $(basename "$0") [-hvila]
 Install pipx and various sets of packages
 Options:
 -i install pipx
--m install main set of packages, includes LSP and other code tools
--o install python-openstackclient
--g install gui desktop programs
--d install DB clients
+-m install python LSP with extensions
 -a install ansible
--t install demo tools
--p install random assortment of optional packages
 -v verbose mode (set -x)
 -h print this message and exit
 "
 
 INSTALL_PIPX=0
-INSTALL_MAIN=0
-INSTALL_DESKTOP=0
-INSTALL_DBCLIENTS=0
-INSTALL_OSC=0
+INSTALL_LSP=0
 INSTALL_ANSIBLE=0
-INSTALL_OPTIONAL=0
-INSTALL_DEMOTOOLS=0
 
-while getopts ':hvimodbatpg' arg; do
+while getopts ':hvila' arg; do
     case "${arg}" in
         i) INSTALL_PIPX=1;;
-        m) INSTALL_MAIN=1;;
-        o) INSTALL_OSC=1;;
-        g) INSTALL_DESKTOP=1;;
-        d) INSTALL_DBCLIENTS=1;;
+        l) INSTALL_LSP=1;;
         a) INSTALL_ANSIBLE=1;;
-        t) INSTALL_DEMOTOOLS=1;;
-        p) INSTALL_OPTIONAL=1;;
         v) set -x;;
         h) echo "$__usage"; exit 0;;
         *) echo "$__usage"; exit 1;;
@@ -152,30 +71,10 @@ if [ "$INSTALL_PIPX" == "1" ]; then
     install_pipx
 fi
 
-if [ "$INSTALL_MAIN" == "1" ]; then
-    install_main
-fi
-
-if [ "$INSTALL_OSC" == "1" ]; then
-    install_osc
-fi
-
-if [ "$INSTALL_DESKTOP" == "1" ]; then
-    install_guitools
-fi
-
-if [ "$INSTALL_DBCLIENTS" == "1" ]; then
-    install_dbclients
+if [ "$INSTALL_LSP" == "1" ]; then
+    install_lsp
 fi
 
 if [ "$INSTALL_ANSIBLE" == "1" ]; then
     install_ansible
-fi
-
-if [ "$INSTALL_OPTIONAL" == "1" ]; then
-    install_optional
-fi
-
-if [ "$INSTALL_DEMOTOOLS" == "1" ]; then
-    install_demotools
 fi
---
+++ scripts/uv-install.sh
@@ -0,0 +1,153 @@
+#!/usr/bin/env bash
+# TODO: re-implement in Ansible somehow
+#
+# NOTE: uv currently does not have an equivalent to pipx's --include-deps
+# so anything that relies on it still should be installed via pipx
+# https://github.com/astral-sh/uv/issues/6314
+
+function install_uv {
+    if command -v brew > /dev/null 2>&1 ; then 
+        brew install uv
+    else
+        # TODO: support other ways of installing
+        echo "Brew is not detected, other installation methods are unsupported"
+        exit 1
+    fi
+}
+
+function install_main {
+    # Python and others development tools
+    uv tool install bindep 
+    uv tool install crudini
+    uv tool install flake8 
+    uv tool install git-review 
+    uv tool install mycli
+    uv tool install pre-commit 
+    uv tool install shellcheck-py 
+    uv tool install sshuttle 
+    uv tool install tox 
+    uv tool install yq
+    # curl with human face
+    uv tool install httpie --with httpie-keystone-auth
+}
+
+function install_osc {
+    # installs a uv env with python-openstackclient and all plugins
+    # for services supported in MOSK
+    # the rest are either non-OpenStack services/components,
+    # OpenStack but do not have HTTP API (like metering (ceilometer)),
+    # or are deprecated in MOSK (like events(panko))
+    # base OSC supports Keystone, Nova, Glance, Cinder, Neutron, Swift
+    uv tool install python-openstackclient \
+        --with python-openstackclient \
+        --with aodhclient \
+        --with gnocchiclient \
+        --with osc-placement \
+        --with python-barbicanclient \
+        --with python-designateclient \
+        --with python-heatclient \
+        --with python-ironicclient \
+        --with python-manilaclient \
+        --with python-masakariclient \
+        --with python-octaviaclient # this one better from downstream for support of force-delete
+    openstack complete > ~/.local/share/bash-completion/completions/openstack
+}
+
+function install_demotools {
+    # demo recording helpers
+    uv tool install asciinema
+    uv tool install demoshell
+    uv tool install doitlive
+}
+
+function install_optional {
+    # file manager a-la lf but in Python
+    uv tool install ranger-fm
+    # markdown files viewer
+    uv tool install frogmouth
+    # log viewer
+    uv tool install toolong
+}
+
+function install_dbclients {
+    # enhanced DB CLI's - https://www.dbcli.com
+    uv tool install pgcli
+    uv tool install litecli
+    uv tool install iredis
+}
+
+function install_guitools {
+    # GUI
+    # posterize pdf or images to multiple sheets that can be glued together
+    uv tool install plakativ
+    # Steam's proton enhancements
+    uv tool install protontricks
+    # rst/md editor with live HTML preview
+    uv tool install retext
+}
+
+__usage="
+Usage: $(basename "$0") [-hvimodbatpg]
+Install uv and various sets of packages
+Options:
+-i install uv
+-m install main set of packages I use
+-o install python-openstackclient
+-g install gui desktop programs
+-d install less used DB clients
+-t install demo tools
+-p install random assortment of optional packages
+-v verbose mode (set -x)
+-h print this message and exit
+"
+
+INSTALL_UV=0
+INSTALL_MAIN=0
+INSTALL_DESKTOP=0
+INSTALL_DBCLIENTS=0
+INSTALL_OSC=0
+INSTALL_OPTIONAL=0
+INSTALL_DEMOTOOLS=0
+
+while getopts ':hvimodbatpg' arg; do
+    case "${arg}" in
+        i) INSTALL_UV=1;;
+        m) INSTALL_MAIN=1;;
+        o) INSTALL_OSC=1;;
+        g) INSTALL_DESKTOP=1;;
+        d) INSTALL_DBCLIENTS=1;;
+        t) INSTALL_DEMOTOOLS=1;;
+        p) INSTALL_OPTIONAL=1;;
+        v) set -x;;
+        h) echo "$__usage"; exit 0;;
+        *) echo "$__usage"; exit 1;;
+    esac
+done
+
+if [ "$INSTALL_UV" == "1" ]; then
+    install_uv
+fi
+
+if [ "$INSTALL_MAIN" == "1" ]; then
+    install_main
+fi
+
+if [ "$INSTALL_OSC" == "1" ]; then
+    install_osc
+fi
+
+if [ "$INSTALL_DESKTOP" == "1" ]; then
+    install_guitools
+fi
+
+if [ "$INSTALL_DBCLIENTS" == "1" ]; then
+    install_dbclients
+fi
+
+if [ "$INSTALL_OPTIONAL" == "1" ]; then
+    install_optional
+fi
+
+if [ "$INSTALL_DEMOTOOLS" == "1" ]; then
+    install_demotools
+fi
