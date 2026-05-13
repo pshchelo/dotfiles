8758424a286456ae663dd7cdf956bf9e7c8c3257
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2026-05-13 20:14:11 +0000

    Add script to backup host before upgrades/rebuilds
    list software installed via various methods, and save some config files.

---
+++ scripts/host-backup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# list installed programs
+ls -al ~/.local/bin > ls-local-bin.txt
+ls /opt > ls-opt.txt
+command -v apt && apt list --installed | grep -v ,autmatic > apt-list-installed.txt
+command -v brew && brew list --installed-on-request > brew-installed-on-request.txt
+command -v uv && uv tool list > uv-tool-list.txt
+command -v snap && snap list > snap-list.txt
+command -v flatpak && flatpack list > flatpack-list.txt
+command -v docker && docker images > docker-images.txt && docker ps -a > docker-ps.txt
+command -v podman && podman images > podman-images.txt && podman ps -a > podman-ps.txt
+command -v lsvirtualenv && lsvirtualenv > lsvirtualenv.txt
+
+# save config files
+tar -caf etc.tar.gz "/etc"
+tar -caf configs.tar.gz "$HOME/.config"
+tar -caf docker.tar.gz "$HOME/.docker"
+# TODO: add more directories as I remember them
